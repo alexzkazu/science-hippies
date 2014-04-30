f4a2876dbea3058e416b43e19ca8a9162b620d0f
alexzkazu <alex.ziebol@gmail.com>
2014-04-29 17:37:07 -0700

    filters work with repeats! yays

--- public/js/filters.js
+++ public/js/filters.js
@@ -1,10 +1,15 @@
 scienceApp.filter('replace', function() {
 	return function (input){
-		return input.replace('stupidity', 'ego');
+		return input.replace('stupidity', 'ego')
+					.replace('test','wee');
 	};
 });
 
 // Controller included to supply data
 scienceApp.controller('stringData', ['$scope', function ($scope) {
-    $scope.string = "Two things are infinite: the universe and human stupidity and I'm not sure about the universe.";
+    $scope.scientists = [
+    {name: "Albert Einstein", picture:"http://http://content8.flixster.com/rtactor/42/23/42230_pro.jpg", quote: "Two things are infinite: the universe and human stupidity and I'm not sure about the universe. test"},
+    {name: "Stephen Hawking", picture:"https://image.guim.co.uk/sys-images/Guardian/About/General/2013/12/6/1386350771830/Stephen-Hawking-009.jpg", quote: "Another quote goes here. Such smart. Much stupidity wow. test"}
+    ];
+
 }]);
